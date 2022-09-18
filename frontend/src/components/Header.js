5d36a6a8aec4dcf1decbfe07fde8270b69edef42
Nihar <niharhegde163@gmail.com>
2022-09-18 12:03:13 +0530

    adding headder, navbar and footer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
   return (
-    <header>Header</header>
-  )
-}
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">GameStore</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/cart">CART</Nav.Link>
+              <Nav.Link href="/login">Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
