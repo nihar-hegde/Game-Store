5380f58d06d3b8d056fac1c546e41bb87d4389fc
Nihar <niharhegde163@gmail.com>
2022-09-19 15:26:36 +0530

    setting up a very simple backend server no database yet

---
+++ backend/data/products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    _id: "1",
+    name: "Battlefield",
+    image: "/images/battlefield.jpg",
+    description: "Mulitplayer war game with may modern wepons and gamemodes",
+    brand: "EA",
+    category: "Game",
+    price: 2999.99,
+    countInStock: 10,
+    rating: 3.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Call of Duty MW2",
+    image: "/images/codmw2.jpg",
+    description:
+      "Call of duty moder warfare 2 with a beautiful campain and engaing mulitplayer",
+    brand: "cod",
+    category: "game",
+    price: 3000.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Forza Horizon 4",
+    image: "/images/forza.jpg",
+    description:
+      "Forza horizon 5 is a realistic racing game with lot of beautiful cars with both online and offline modes and it is openworld",
+    brand: "forza",
+    category: "game",
+    price: 4000.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Sony Playstation 4 Pro White Version",
+    image: "/images/playstation.jpg",
+    description:
+      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    brand: "Sony",
+    category: "Electronics",
+    price: 50000.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 6000.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "valorant",
+    image: "/images/valorant.jpg",
+    description:
+      "Valorant is a free to play game with very engaging gamemodes like 5v5 and many more",
+    brand: "Riot",
+    category: "game",
+    price: "Free",
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+module.exports = products;
